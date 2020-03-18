d5b300216e7c2a6ae11e695a56745d8bc0413ea4
John Davis <johnedavis624@gmail.com>
2020-03-17 21:25:11 -0500

    got seed.sql to pre populate

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE department (
-    id INT NOT NULL,
-    name VACHAR(30) NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NULL,
     salary DECIMAL(10,4) NULL,
     department_id INT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NULL,

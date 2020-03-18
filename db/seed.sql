7dc88eaeae9749a278ae919a670f685f35ea5370
John Davis <johnedavis624@gmail.com>
2020-03-17 21:14:01 -0500

    built all command line apps except view role and add departments

---
+++ db/seed.sql
@@ -0,0 +1,38 @@
+USE employee_trackerDB;
+
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Be", "Yonce", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Clyde", "Eversteen", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Old", "Gregg", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Malick", "Peterson", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Rodney", "Brown", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dolly", "Patron", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Lady", "Blye", 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Gula Gula", "Island", 1, 2);
